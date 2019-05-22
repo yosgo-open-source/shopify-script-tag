f9f9d03dec5292425984529fa42598b1d828f42a
kcin1993 <kcin1993@gmail.com>
2019-05-22 22:10:22 +0800

    Update dev js

---
+++ README.md
@@ -0,0 +1,8 @@
+### Add following to test store
+
+- JS: https://cdn.jsdelivr.net/gh/yosgo-open-source/shopify-script-tag/jioukou-dev.js
+- Store Group buy example: https://yosgo-store.myshopify.com/pages/group-buy?groupId=8HHrxzPCB
+
+### Add following to production store
+
+https://cdn.jsdelivr.net/gh/yosgo-open-source/shopify-script-tag/jioukou.js
--- jioukou-dev.js
+++ jioukou-dev.js
@@ -1,1 +1,39 @@
-console.log("working");
+(function() {
+  /**
+   * Config
+   */
+  var Group_buy_url =
+    "https://shopify-app-development.yosgo.com" + "/clients?groupId=";
+  var ParamName = "groupId";
+  var PageName = "group-buy";
+  var PageId = "PageContainer";
+
+  /**
+   * 1. Read window url. Check url is Shopify group buy page or not
+   * 2. Parse group buy page to get groupId from Shopify store webiste
+   * 3. Insert Jioukou app into Shopify group buy page
+   */
+
+  function qs(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)");
+    var results = regex.exec(location.search);
+    return results === null
+      ? ""
+      : decodeURIComponent(results[1].replace(/\+/g, " "));
+  }
+
+  if (window.location.href.indexOf(PageName) >= 0) {
+    if (qs(ParamName)) {
+      var page = document
+        .createElement("iframe")
+        .setAttribute("style", "width: 100%; border: 0px; height: 100vh;")
+        .setAttribute("src", Group_buy_url + groupId);
+      document.getElementById(PageId).appendChild(page);
+    } else {
+      alert(
+        "Can't read group ID. Please check webiste url or page is set with right way"
+      );
+    }
+  }
+})();
