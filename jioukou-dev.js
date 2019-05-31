052b02d1a3fcf11fe1bb54bb738d021c69e2ca37
kcin1993 <kcin1993@gmail.com>
2019-05-31 16:45:05 +0800

    update

--- jioukou-dev.js
+++ jioukou-dev.js
@@ -1,11 +1,13 @@
 /** v1.0.0 */
 (function() {
-  /** Config */
+  /** Env config */
   var Group_buy_url =
     "https://shopify-app-development.yosgo.com" + "/group-buy?groupId=";
+  var API_URL = "https://shopify-api-development.yosgo.com/graphql";
+
+  /** Common config */
   var Param_groupId = "groupId"; /**網址上，用於的揪團 ID */
-  var Param_tagType = "tagType"; /**網址上，用於承裝 Iframe 容器的類別 */
-  var Param_tagName = "tagName"; /**網址上，用於承裝 Iframe 容器的名稱 */
+  var Param_shopId = "shopId"; /**網址上，用於查詢商店基本資料 */
   var PageName = "group-buy";
 
   function qs(name) {
@@ -19,41 +21,54 @@
 
   /**僅在 group-buy 的 page 內作用 */
   if (window.location.href.indexOf(PageName) >= 0) {
-    /**確認 groupId 是否存在 */
-    if (qs(Param_groupId)) {
-      /**建立 iframe 元件 */
-      var page = document.createElement("iframe");
-      page.setAttribute("src", Group_buy_url + qs(Param_groupId));
-      page.setAttribute(
-        "style",
-        "width: 100%; border: 0px; min-height: 100vh;"
-      );
-      /**決定 Iframe 位置 */
-      if (
-        qs(Param_tagName) &&
-        qs(Param_tagType) &&
-        qs(Param_tagName) !== "" &&
-        qs(Param_tagType) !== ""
-      ) {
-        if (qs(Param_tagType) === "class") {
-          /**Iframe 於 class 的元件中*/
-          var ele = document.getElementsByClassName(qs(Param_tagName));
-          ele.innerHTML = "";
-          ele.appendChild(page);
-          console.log("[Jioukou loaded in class element]");
-        } else if (qs(Param_tagType) === "id") {
-          /**Iframe 於 id 的元件中*/
-          var ele = document.getElementById(qs(Param_tagName));
-          ele.innerHTML = "";
-          ele.appendChild();
-          console.log("[Jioukou loaded in id element]");
-        }
-      } else {
-        /**Iframe 於 body  */
-        document.body.innerHTML = "";
-        document.body.appendChild(page);
-        console.log("[Jioukou loaded in body]");
-      }
+    /**確認 groupId, shopId 是否存在 */
+    if (qs(Param_groupId) && qs(Param_shopId)) {
+      /**查詢 shop 資料 */
+      fetch(API_URL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          query:
+            '{  getShopSettings(shopId: "' +
+            qs(Param_shopId) +
+            '.myshopify.com") {    tag_type    tag_name    language    message    errors  }}'
+        })
+      })
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(jsonData) {
+          var tag_name = jsonData.data.getShopSettings.tag_name;
+          var tag_type = jsonData.data.getShopSettings.tag_type;
+          /**建立 iframe 元件 */
+          var page = document.createElement("iframe");
+          page.setAttribute("src", Group_buy_url + qs(Param_groupId));
+          page.setAttribute(
+            "style",
+            "width: 100%; border: 0px; min-height: 100vh;"
+          );
+          /**決定 iframe 位置 */
+          if (tag_type === "body") {
+            /**Iframe 於 body  */
+            document.body.innerHTML = "";
+            document.body.appendChild(page);
+            console.log("[Jioukou loaded in body]");
+          } else {
+            if (tag_type === "id") {
+              var element = document.body.getElementsByClassName(tag_name)[0];
+              element.innerHTML = "";
+              element.appendChild(page);
+              console.log("[Jioukou loaded in class]");
+            } else if (tag_type === "class") {
+              var element = document.body.getElementById(tag_name);
+              element.innerHTML = "";
+              element.appendChild(page);
+              console.log("[Jioukou loaded in id]");
+            }
+          }
+        });
     } else {
       alert("GroupId is missing. It might wrong with url");
     }
